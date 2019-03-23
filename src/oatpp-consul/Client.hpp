2e8b42e4e5dafd82a137030fcf06f9ab8c7263d4
lganzzzo <lganzzzo@gmail.com>
2019-03-23 18:50:53 +0200

    better API docs

--- src/oatpp-consul/Client.hpp
+++ src/oatpp-consul/Client.hpp
@@ -38,27 +38,44 @@ private:
   template<class T>
   using List = oatpp::data::mapping::type::List<T>;
 public:
-  
+
+  /**
+   * Client call error.
+   */
   class Error : public std::runtime_error {
   private:
     oatpp::String m_message;
     v_int32 m_httpErrorCode;
   public:
-    
+
+    /**
+     * Constructor.
+     * @param message - error message.
+     * @param httpErrorCode - http error code.
+     */
     Error(const oatpp::String& message, v_int32 httpErrorCode)
       :std::runtime_error(message->c_str())
       , m_message(message)
     {}
-    
+
+    /**
+     * Get error message.
+     * @return - &id:oatpp::String;.
+     */
     oatpp::String& getMessage() {
       return m_message;
     }
-    
+
+    /**
+     * Get http error code.
+     * @return - http error code.
+     */
     v_int32 getHttpErrorCode() {
       return m_httpErrorCode;
     }
     
   };
+
 private:
   std::shared_ptr<oatpp::data::mapping::ObjectMapper> m_objectMapper;
   std::shared_ptr<rest::Client> m_restClient;
@@ -80,26 +97,31 @@ public:
   
   /**
    *  Get metadata for key containing base64 encoded value along with other fields
+   *  @throws - &l:Client::Error;.
    */
   rest::KVMetadata::ObjectWrapper kvGetMetadata(const oatpp::String& key) const;
   
   /**
    *  Same as kvGetMetadata(key) but for given datacenter (DC)
+   *  @throws - &l:Client::Error;.
    */
   rest::KVMetadata::ObjectWrapper kvGetMetadataInDC(const oatpp::String& key, const oatpp::String& datacenter) const;
   
   /**
    *  Get value by key
+   *  @throws - &l:Client::Error;.
    */
   oatpp::String kvGet(const oatpp::String& key) const;
   
   /**
    *  Get value by key for given datacenter
+   *  @throws - &l:Client::Error;.
    */
   oatpp::String kvGetInDC(const oatpp::String& key, const oatpp::String& datacenter) const;
   
   /**
    *  Get value by key and deserialize it to DTO object using given objectMapper
+   *  @throws - &l:Client::Error;.
    */
   template<class Type>
   typename Type::ObjectWrapper kvGetAsDto(const oatpp::String& key, const std::shared_ptr<oatpp::data::mapping::ObjectMapper>& objectMapper) const {
@@ -112,6 +134,7 @@ public:
   
   /**
    *  Get value by key for given datacenter and deserialize it to DTO object using given objectMapper
+   *  @throws - &l:Client::Error;.
    */
   template<class Type>
   typename Type::ObjectWrapper kvGetAsDtoInDC(const oatpp::String& key,
@@ -126,21 +149,25 @@ public:
   
   /**
    *  Put value for key
+   *  @throws - &l:Client::Error;.
    */
   bool kvPut(const oatpp::String& key, const oatpp::String& value) const;
   
   /**
    *  Put value for key for given datacenter
+   *  @throws - &l:Client::Error;.
    */
   bool kvPutInDC(const oatpp::String& key, const oatpp::String& value, const oatpp::String& datacenter) const;
   
   /**
    *  Delete value for key
+   *  @throws - &l:Client::Error;.
    */
   bool kvDelete(const oatpp::String& key) const;
   
   /**
    *  Delete value for key for given datacenter
+   *  @throws - &l:Client::Error;.
    */
   bool kvDeleteInDC(const oatpp::String& key, const oatpp::String& datacenter) const;
   
