0a4d8f3829b255f6900964c41e4cacfcc5a224b3
Marco Leclerc <marco.leclerc@radio-ip.com>
2019-11-21 11:21:23 -0500

    added some documentation

--- src/oatpp-consul/rest/DTOs.hpp
+++ src/oatpp-consul/rest/DTOs.hpp
@@ -276,13 +276,50 @@ class AgentServiceRegisterPayload : public oatpp::data::mapping::type::Object {
  */
 class SessionPayload : public oatpp::data::mapping::type::Object {
   DTO_INIT(SessionPayload, Object)
+
+  /**
+   * Internal index value representing when the service was created
+  */
   DTO_FIELD(Int64, createIndex, "CreateIndex");
+
+  /**
+   *  ID of the session
+  */
   DTO_FIELD(String, id, "ID");
+
+  /**
+   *  Specifies the duration for the lock delay. This must be greater than 0.
+  */
   DTO_FIELD(Int64, lockDelay, "LockDelay");
+
+  /**
+   * Specifies a human-readable name for the session.
+  */
   DTO_FIELD(String, name, "Name");
+
+  /**
+   *  Specifies the name of the node. 
+  */
   DTO_FIELD(String, node, "Node");
+
+  /**
+   * Specifies a list of associated health check IDs
+  */
   DTO_FIELD(List<String>::ObjectWrapper, cheks, "Checks");
+
+  /**
+   * Controls the behavior to take when a session is invalidated. Valid values are:
+   *   release - causes any locks that are held to be released
+   *   delete - causes any locks that are held to be deleted
+  */
   DTO_FIELD(String, behavior, "Behavior");
+
+  /**
+   * Specifies the number of seconds (between 10s and 86400s). 
+   * If provided, the session is invalidated if it is not renewed before the TTL expires. 
+   * The lowest practical TTL should be used to keep the number of managed sessions low. 
+   * When locks are forcibly expired, such as when following the leader election pattern in an application, sessions may not be reaped for up to double this TTL, so long TTL values (> 1 hour) should be avoided.
+  */
   DTO_FIELD(String, ttl, "TTL");
 };
 
@@ -305,23 +342,80 @@ class WeightsPayload : public oatpp::data::mapping::type::Object {
  */
 class CatalogServicePayload : public oatpp::data::mapping::type::Object {
   DTO_INIT(CatalogServicePayload, Object)
+
+  /**
+   * An optional UUID to assign to the node. This must be a 36-character UUID-formatted string.
+  */
   DTO_FIELD(String, id, "ID");
+
+  /*
+  *  Name of the Consul node on which the service is registered
+  */
   DTO_FIELD(String, node, "Node");
+  /**
+   *  IP address of the Consul node on which the service is registered.
+   */
   DTO_FIELD(String, address, "Address");
+
+  /**
+   *  Data center of the Consul node on which the service is registered.
+   */
+
   DTO_FIELD(String, datacenter, "Datacenter");
+  /**
+   * List of explicit LAN and WAN IP addresses for the agent
+   * */
   DTO_FIELD(Fields<String>::ObjectWrapper, taggedAddresses, "TaggedAddresses"); 
+
+  /**
+  * List of user-defined metadata key/value pairs for the node
+  */
   DTO_FIELD(Fields<String>::ObjectWrapper, nodeMeta, "NodeMeta");
+
+  /**
+   * Unique service instance identifier
+  */
   DTO_FIELD(String, serviceId, "ServiceID");
+
+  /**
+   * Name of the service
+  */
   DTO_FIELD(String, serviceName, "ServiceName");
+
+  /**
+   * IP address of the service host — if empty, node address should be used
+  */
   DTO_FIELD(String, serviceAddress, "ServiceAddress");
 
+  /**
+   * Map of explicit LAN and WAN addresses for the service instance. This includes both the address as well as the port.
+  */
   DTO_FIELD(Fields<String>::ObjectWrapper, serviceTaggedAddresses, "ServiceTaggedAddresses");
 
+  /*
+  * List of tags for the service
+  */
   DTO_FIELD(List<String>::ObjectWrapper, serviceTags, "ServiceTags");
+
+  /*
+  * List of user-defined metadata key/value pairs for the service
+  */
   DTO_FIELD(Fields<String>::ObjectWrapper, serviceMeta, "ServiceMeta");
+
+  /*
+  * Port number of the service
+  */
   DTO_FIELD(Int32, servicePort, "ServicePort");
   DTO_FIELD(WeightsPayload::ObjectWrapper, serviceWeights, "ServiceWeights");
+
+  /*
+  *  Indicates whether service tags can be overridden on this service
+  */
   DTO_FIELD(Boolean, sServiceEnableTagOverride, "ServiceEnableTagOverride");
+
+  /*
+  * Internal index value representing when the service was created
+  */
   DTO_FIELD(Int64, createIndex, "CreateIndex");
 
 };
